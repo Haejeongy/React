d5fc0e5d13e3944270a846fd65970605101d4d0c
haejeongy <haejeongy.922@gmail.com>
2022-06-23 22:31:03 +0900

    0623 TIL
    React

---
+++ react_blog/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'; //react core function 기능이 들어있는 코드 npm으로 깔아줄 때 자동으로 깔립니다.
+import ReactDOM from 'react-dom/client'; //리액트랑 돔을 연결해주는 코드
+import './index.css'; //기본적인 css 파일을 가져옵니다
+import App from './App'; //App 파일을 가지고 옵니다
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
