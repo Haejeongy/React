b10e2d8b8ff217d7f49c3a625335be0829945431
haejeongy <haejeongy.922@gmail.com>
2022-06-30 23:56:37 +0900

    React storybook/state

---
+++ practice/src/App.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [time, setTime] = useState(1);
+  
+  const handleClick = () => {
+    setTime(time+1);
+  };
+
+  return (
+    <div>
+      <span>현재 시각:{time}시</span>
+      <button onClick={handleClick}>update</button>
+    </div>
+  );
+}
+
+export default App;
