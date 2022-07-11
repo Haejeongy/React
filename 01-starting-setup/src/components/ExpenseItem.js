c183415f604db392e113ba870da777a9b08aa051
haejeongy <haejeongy.922@gmail.com>
2022-07-11 22:23:32 +0900

    Concept of Composition

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 //리액트에 있는 컴포넌트는 오직 자바스크립트 함수다
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from './Card';
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- TIL/0711 TIL.js
+++ TIL/0711 TIL.js
@@ -1,1 +1,28 @@
-// function of React always need return !! for rendering 
+// function of React always need return !! for rendering
+
+// concept of composition
+// 컴포넌트 사이에서 중복되는 css 스타일이나, 요소들을
+// 부모 컴포넌트로 빼주고 -> 부모 컴포넌트로 만들어서 div 대신 사용할 수 있다.
+// 대신 프롭스를 내리고, 태그 사이에 {props.children}이라고 써주기
+// 그럼 이 컴포넌트로 싸여진 다른 컴포넌트들은 자동적으로 children이 된다.
+// CSS 부분 이해 못함 section3 40 , 10분~
+
+// concept of JSX
+// 브라우저는 JSX코드를 지원하지않음!
+// react / react-dom 이 뒤에서 어떤일을 하는가?
+
+// return (
+//     <div>
+//         <h2>let's go</h2>
+//         <Expenses items={expenses} />
+//     </div>
+// );
+
+// 이 html 코드를 내장 react dependency가 바꿔준다
+// return React.createElement(
+//     'div',{}
+//     React.createElement('h2',{}, "let's go"),
+//     React.createElement(Expenses, { items:expenses})
+// );
+
+// wrapper을 꼭 가져야 하는 이유
