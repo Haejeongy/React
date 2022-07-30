7db954f04c132cc77557c98260d4e97e0c270272
haejeongy <haejeongy.922@gmail.com>
2022-07-30 15:18:16 +0900

    React usestate

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -2,23 +2,35 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
+
+// 의사코드
+// 버튼을 누를 때마다 타이틀이 바뀌게 하고싶다
+// useState, 기본값 = "원래 타이틀"
+// 버튼을 클릭할때 상태를 변경하는 함수 실행
+// onClick시 실행되는 함수 안에, setTitle 함수 (바꾸고싶은 상태)를 넣어줌
+// state는 항상 함수컴포넌트 안에 작성
+// 프롭스를 변수로 바꿔준다
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const [back, setBack] = useState(false);
+
   const clickHandler = () => {
-    console.log('CLICKED!!')
-  }
+    setTitle("Updated!");
+    setBack(props.title);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={}></button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
 
 export default ExpenseItem;
-
