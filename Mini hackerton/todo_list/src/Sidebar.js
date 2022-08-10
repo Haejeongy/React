73a39c9c30427051109bebfabe29cd7bcec01dc1
haejeongy <haejeongy.922@gmail.com>
2022-08-11 00:35:31 +0900

    Update Styled component

--- Mini hackerton/todo_list/src/Sidebar.js
+++ Mini hackerton/todo_list/src/Sidebar.js
@@ -1,11 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 
-const Sidebar = () => {
+export const Styledicon = styled.div`
+  .icons {
+  width: 30px;
+  height: 30px;
+  display: flex;
+  }
+`;
+
+export const Sidebar = () => {
   return (
     <section>
-      <img src="https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon3/ul-pen.svg"></img>
-      <img src="https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon3/ul-diary.svg"></img>
-      <img src="https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon3/ul-setting.svg"></img>
+      <Styledicon>
+        <div>
+          <img
+            className="icons"
+            src="https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon3/ul-pen.svg"
+            alt="icon"
+          ></img>
+          <img
+            className="icons"
+            src="https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon3/ul-diary.svg"
+            alt="icon"
+          ></img>
+          <img
+            className="icons"
+            src="https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon3/ul-setting.svg"
+            alt="icon"
+          ></img>
+        </div>
+      </Styledicon>
     </section>
   );
 };
