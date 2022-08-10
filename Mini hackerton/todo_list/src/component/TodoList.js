57d49676e3eb649faac197ae10287b7d4d3d0c07
haejeongy <haejeongy.922@gmail.com>
2022-08-10 23:27:33 +0900

    Create TodoList Component

---
+++ Mini hackerton/todo_list/src/component/TodoList.js
@@ -0,0 +1,15 @@
+function TodoList() {
+  return (
+    <section>
+      <div>
+        <p>감사일기</p>
+      </div>
+      <div>
+        <input></input>
+        <button>submit</button>
+      </div>
+    </section>
+  );
+}
+
+export default TodoList;
