a970dd81628de7a241f89568047df53956b79c5a
haejeongy <haejeongy.922@gmail.com>
2022-08-10 23:28:01 +0900

    Create Sidebar.js

---
+++ Mini hackerton/todo_list/src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <section>
+      <i className="https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon3/ul-diary.svg"></i>
+    </section>
+  );
+};
+
+export default Sidebar;
